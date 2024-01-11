73536cd070b7614a0aa285a5ff8f83e8527f1c8e
LucasMonteiro10 <lmvb@discente.ifpe.edu.br>
2024-01-11 12:08:58 -0300

    Cria projeto para testar manipulação de strings

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RoboAndarilho.Models;
+class Program
+{
+    static void Main()
+    {
+        string velocidades = Console.ReadLine();
+
+        int vmin = int.Parse(velocidades.Split(' ')[0]);
+        int vmax = int.Parse(velocidades.Substring(velocidades.LastIndexOf(' ') + 1));
+
+        Robo r = new Robo(vmin, vmax);
+
+        string comandos = Console.ReadLine();
+
+        for (int i = 0; i < comandos.Length; i++)
+        {
+            switch (comandos[i])
+            {
+                case 'A':
+                    r.Acelerar();
+                    break;
+                case 'D':
+                    r.Desacelerar();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Console.WriteLine($"{r.VelocidadeAtual}");
+    }
+}
